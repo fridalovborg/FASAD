8a8dd9de5dc8a431240943a4ecb4415d84dbaad4
Frida Lövborg <frida.lovborg@sigma.se>
2020-02-06 21:18:28 +0100

    added child menu

--- wp-content/themes/fasad2019/home.php
+++ wp-content/themes/fasad2019/home.php
@@ -4,19 +4,23 @@
     <h1 class="fasad-main__title"><?php single_post_title(); ?></h1>
 
     <div class="fasad-news">
+        <ol class="fasad-news__list">
         <?php
             if ( have_posts() ) {
                 while ( have_posts() ) : the_post();
                     $post_page_link = get_the_permalink();
-                    echo '<div class="fasad-news__item">';
-                    echo '<a class="fasad-news__item-link" href="' . $post_page_link . '">';
-                    the_title('<h2 class="fasad-news__item-title">', '</h2>');
-                    echo excerpt(50);
+                    echo '<li class="fasad-news__list-item">';
+                    echo '<span class="fasad-news__content">';
+                    echo '<a class="fasad-news__content-link" href="' . $post_page_link . '">';
+                    the_title('<h2 class="fasad-news__content-title">', '</h2>');
+                    echo '<p>' . excerpt(50) . '</p>';
                     echo '</a>';
-                    echo '</div>';
+                    echo '</span>';
+                    echo '</li>';
                 endwhile;
             }
         ?>
+        </ol>
     </div> <!-- END: .fasad-news -->
 <?php
     get_footer();
--- wp-content/themes/fasad2019/index.php
+++ wp-content/themes/fasad2019/index.php
@@ -2,6 +2,24 @@
     get_header();
 
         if ( have_posts() ) {
+
+            // child menu
+            if ( !$post->post_parent ) {
+                $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+            } else {
+                if ( $post->ancestors ) {
+                    $ancestors = end($post->ancestors);
+                    $children = wp_list_pages("title_li=&child_of=".$ancestors."&echo=0");
+                }
+            }
+            
+            if ( $children ) {
+                echo '<nav class="child-menu">';
+                echo '<ul class="child-menu__list">' . $children . '</ul>';
+                echo '</nav>';
+            }
+
+            // post content
             while ( have_posts() ) : the_post();
                 the_title('<h1 class="fasad-main__title">', '</h1>');
                 the_content();
